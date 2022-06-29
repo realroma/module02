31806ecbce0d74303e355a1dc1466f4e07d02013
realroma <real3060@yandex.ru>
2022-06-29 18:04:45 -0400

    rework

--- 02.04.go
+++ 02.04.go
@@ -2,19 +2,24 @@ package main
 
 import "fmt"
 
-func main () { 
-	f := []string{"zachem eto?"} 
+func main () {
+	f := []string{"zachem eto?"}
 	var client map[string]map[string]string = map[string]map[string]string{
 	"Anatoliy Fosjv": {
 		"Geroi ne ricari i magi": "1",
 		"ne moloko:sostav": "1",
 		"nas ne dogonyat": f[0]},
+	"fostr agilw": {
+		"fogot": "1",},
 	}
 	fmt.Println(client)
-	books := make([]string, 0, len(client["Anatoliy Fosjv"]))
-	for k := range client["Anatoliy Fosjv"]{
+	books := make([]string, 0, 0)
+	for t := range client{
+		fmt.Println(t)
+		books = make ([]string, 0, len(client[t]))
+		for k := range client[t]{
 		books = append(books, k)
+		}
+	fmt.Println(books)
 	}
-	
-	fmt.Println(len(books))
 }
