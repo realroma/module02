0715cee50a0891f1bbca8d21a4604dcb96121625
realroma <real3060@yandex.ru>
2022-07-21 16:11:51 -0400

    1 chapter

---
+++ 02.05.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func contains (a []string, x string) (res bool){
+	for _, q :=  range a{
+		if q == x {
+			return true
+		}
+	}
+	return res
+}
+func main(){
+	var a []string
+	a = []string{"i", "know", "where"}
+	x := "you"
+	fmt.Println(contains(a, x))
+	a = []string{"i", "love", "you"}
+	fmt.Println(contains(a, x))
+}
