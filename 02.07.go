abcd3a5bde3664c1c337f132bf2010139ef3a411
realroma <real3060@yandex.ru>
2022-10-04 12:47:54 -0400

    add writing text after reading

--- 02.07.go
+++ 02.07.go
@@ -7,22 +7,26 @@ import (
 )
 
 func write (in string, out string) {
-	InFile, _ := os.Open(in)
-        OutFile, _ := os.Create(out)
-	defer InFile.Close()
-	defer OutFile.Close()
-	reader := bufio.NewScanner(InFile)
-        writer := bufio.NewWriter(OutFile)
-	defer writer.Flush()
+	InFile, _ := os.Open(in)// Открываем файл для импорта данных
+        OutFile, _ := os.Create(out)// Создаём файл для экспорта данных
+	defer InFile.Close()// Закрываем файл импорта после исполнения кода
+	defer OutFile.Close()// Закрываем файл экспорта после исполнения кода
+	reader := bufio.NewScanner(InFile)// Создаём сканер для считывания
+        writer := bufio.NewWriter(OutFile)// Создаём сканер для записи
+	defer writer.Flush()// Записываем данные из буфера в файл
+	var text string //Инициализируем переменную
         for reader.Scan() {
 		if reader.Text() == "" {
-			panic ("Plase is empty")
+			panic ("Plase is empty")//Если количество байт в строке 0 вызвать панику
 		}
-                writer.WriteString(reader.Text() + "\n")
+               	writer.WriteString(reader.Text() + "\n")//Загружаем строку в буфер
+		fmt.Println(string(reader.Text))
+		text = reader.Text()//Присваиваем возвращаемую функцию в переменную
+		fmt.Println(text)//Выводим результат функции
         }
 }
 
+
 func main() {
 	write("InFile.txt", "OutFile")
-	fmt.Println("Done")
 }
